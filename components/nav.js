ec0b35c43512620816b93d40e16fb8bd01d457b2
Xiao Hanyu <xiaohanyu1988@gmail.com>
2019-01-11 00:13:22 +0800

    Add logo image to nav component

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Container, Grid, Header } from "semantic-ui-react";
+import { Container, Grid, Header, Image } from "semantic-ui-react";
 
 export default class Nav extends React.Component {
   render() {
@@ -11,11 +11,16 @@ export default class Nav extends React.Component {
 
     return (
       <div>
-        <Container text style={{ paddingTop: "3rem", paddingBottom: "1rem" }}>
-          <Header as="h1">
-            <Header.Content>{this.props.title}</Header.Content>
-            <Header.Subheader>{this.props.subtitle}</Header.Subheader>
-          </Header>
+        <Container text style={{ paddingTop: "2rem", paddingBottom: "2rem" }}>
+          <Grid
+            style={{ alignItems: "center", marginBottom: "0", marginTop: "0" }}
+          >
+            <Image src="/static/images/logo.svg" size="small" />
+            <Header as="h1" style={{ marginTop: "0" }}>
+              <Header.Content>{this.props.title}</Header.Content>
+              <Header.Subheader>{this.props.subtitle}</Header.Subheader>
+            </Header>
+          </Grid>
         </Container>
         <Grid padded columns={7}>
           <Grid.Column color="red" style={gridColumnStyle} />
